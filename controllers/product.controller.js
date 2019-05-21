7b3a5956c64535afe949708716d605e39cadcd54
Abhishek singh <abhisheksingh@Appss-Mac-mini.local>
2019-05-21 12:05:37 +0530

    created the update, delete and find API.

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -20,5 +20,23 @@ module.exports = {
       if(err) throw err;
       res.send({data:response,message:'Product found'} )
     })
+  },
+  product_update: function(req,res){
+    Product.findByIdAndUpdate(req.params.id,{$set:req.body},function(err,product){
+      if(err) return next(err);
+      res.send('Product updated successfully');
+    })
+  },
+  product_delete: function(req,res){
+    Product.findByIdAndRemove(req.params.id,function(err){
+      if(err) return next(err);
+      res.send('Product deleted successfully')
+    })
+  },
+  product_list: function(req,res){
+    Product.find(function(err,response){
+      if(err) return next(err)
+      res.send({data:response,message:'Success'})
+    })
   }
 }
