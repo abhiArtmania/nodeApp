f055e357698e0dad34a1283bbfbdc3d7caed88c5
Abhishek singh <abhisheksingh@Appss-Mac-mini.local>
2020-01-09 17:27:23 +0530

    changes

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,39 +5,47 @@ module.exports = {
     //res.render('index', { title: 'Hey', message: 'Hello there!' })
   },
   product_create: function(req,res){
-    let product = new Product({
+    Product.create({
       name:req.body.name,
       price:req.body.price
-    });
-    product.save(function(err, response){
-      if(err){
-        return next(err)
-      }
+    }).then(function(response){
+      console.log(response,"-----response");
       res.send({data: response, msg: "product saved"})
+    }).catch(function(error){
+      console.log(error,"------error");
+      throw error;
     })
   },
   product_details: function(req,res){
-    Product.findById(req.params.id,function(err,response){
-      if(err) throw err;
-      res.send({data:response,message:'Product found'} )
+    Product.findOne({
+      _id:req.params.id
+    }).then(function(response){
+      console.log(response,"---response");
+      res.send({data:response,message:'Product found'})
+    }).catch(function(error){
+      console.log(error,"------error");
+      throw error;
     })
   },
   product_update: function(req,res){
-    Product.findByIdAndUpdate(req.params.id,{$set:req.body},function(err,product){
-      if(err) return next(err);
-      res.send('Product updated successfully');
+    Product.update({_id:req.params.id},{$set:req.body}).then((response)=>{
+      res.send('Product updated successfully')
+    }).catch((error)=>{
+      throw error;
     })
   },
   product_delete: function(req,res){
-    Product.findByIdAndRemove(req.params.id,function(err){
-      if(err) return next(err);
+    Product.remove({_id:req.params.id}).then((response)=>{
       res.send('Product deleted successfully')
+    }).catch((error)=>{
+      throw error;
     })
   },
   product_list: function(req,res){
-    Product.find(function(err,response){
-      if(err) return next(err)
+    Product.find().then((response)=>{
       res.send({data:response,message:'Success'})
+    }).catch((error)=>{
+      throw error;
     })
   },
   product_pagination: function(req,res){
@@ -45,13 +53,13 @@ module.exports = {
     let size = parseInt(req.query.size)
     let response = {}
     let query = {}
-    if( !pageNo || pageNo < 0 || pageNo === 0 ){
+    if( !(pageNo*1) || pageNo*1 < 1 ){
       response = {
         error: true,
         message: 'Invalide page number, should start with 1'
       }
       return res.json(response)
-    } else if ( !size || size === 0 ){
+    } else if ( !size*1 ){
       response = {
         error: true,
         message: 'Invalide size'
@@ -60,31 +68,40 @@ module.exports = {
     } else {
       query.skip = size * ( pageNo - 1 )
       query.limit = size
-      Product.count({},function(err,totalCount){
-        if(err){
-          return res.json({
-            error:true,
-            message:'Error fetching data'
-          })
-        } else {
-          let totalPage = Math.ceil(totalCount/size)
-          Product.find({},{},query,function(err,data){
-            if(err){
-              response = {
-                error: true,
-                message: 'Error fetching data'
-              }
-            } else {
-              response = {
-                error:  false,
-                message:  data,
-                totalPages: totalPage
-              }
-            }
-            res.json(response)
-          })
-        }
+      Product.find({}).skip(query.skip).limit(query.limit).lean().then((response)=>{
+        let final = {};
+        final['data'] = response;
+        final['count'] = response.length;
+        res.json(final)
+      }).catch((error)=>{
+        throw error;
       })
+
+      // Product.count({},(err,totalCount)=>{
+      //   if(err){
+      //     return res.json({
+      //       error:true,
+      //       message:'Error fetching data'
+      //     })
+      //   } else {
+      //     let totalPage = Math.ceil(totalCount/size)
+      //     Product.find({},{},query,function(err,data){
+      //       if(err){
+      //         response = {
+      //           error: true,
+      //           message: 'Error fetching data'
+      //         }
+      //       } else {
+      //         response = {
+      //           error:  false,
+      //           message:  data,
+      //           totalPages: totalPage
+      //         }
+      //       }
+      //       res.json(response)
+      //     })
+      //   }
+      // })
     }
   }
 }
